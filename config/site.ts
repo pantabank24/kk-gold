44df61ebc7f9eed6171cd3f1cbbe5e78a58ec8fd
pantabank <panthakarn.cpe@gmail.com>
2025-12-25 01:45:35 +0700

    first initial

---
+++ config/site.ts
@@ -0,0 +1,47 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "KK หลอมทอง | เช็คราคาทอง ตรวจราคาทอง ประเมินราคาทอง",
+  url: "https://watcharagold.com/",
+  description: "KK หลอมทอง ตรวจราคาทอง เช็คราคาทอง ประเมินราคาทองได้ที่นี่ | รับซื้อทองคำทุกประเภท โทร 0639289393 หรือเดินทางมาที่ร้าน https://maps.app.goo.gl/msZZVXprEYpYWU3z5",
+  navItems: [
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    line: "https://lin.ee/XukDRz8",
+    facebook: "https://www.facebook.com/kkgoldhatyai"
+  },
+};
